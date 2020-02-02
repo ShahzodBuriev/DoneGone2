57349694d9e115e48a96f6d5385c0454776afd18
Shahzod <shakhzodb1@gmail.com>
2020-02-02 21:38:59 +0500

    Initial Commit

---
+++ DoneGone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoneGone
+//
+//  Created by Shahzod Buriev on 2/2/20.
+//  Copyright © 2020 Shahzod Buriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
