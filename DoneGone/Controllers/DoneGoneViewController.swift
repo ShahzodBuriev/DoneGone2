a8ff4de9ebd67f26878e2daba9a0520e025da349
Shahzod <shakhzodb1@gmail.com>
2020-02-06 15:59:18 +0500

    Latest updates

---
+++ DoneGone/Controllers/DoneGoneViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  DoneGone
+//
+//  Created by Shahzod Buriev on 2/2/20.
+//  Copyright © 2020 Shahzod Buriev. All rights reserved.
+//
+
+import UIKit
+
+class DoneGoneViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let defults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        
+        let newItem = Item()
+        newItem.title = "Mike Posner"
+        itemArray.append(newItem)
+        
+             let newItem1 = Item()
+               newItem1.title = "Otash"
+               itemArray.append(newItem1)
+               
+        
+        let newItem2 = Item()
+               newItem2.title = "Bahti"
+               itemArray.append(newItem2)
+               
+        
+        let newItem3 = Item()
+               newItem3.title = "Shams"
+               itemArray.append(newItem3)
+               
+        
+        super.viewDidLoad()
+        
+//     if let items = defults.array(forKey: "ToDoListArray") as? [String]
+//     {
+//      itemArray = items
+//       }
+//
+    }
+    
+    
+       override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+   
+      override  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListItemCell", for: indexPath)
+        
+            cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        if itemArray[indexPath.row].done == true
+        {
+            cell.accessoryType  =  .checkmark
+        }
+        else
+        {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+      
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+            
+            if itemArray[indexPath.row].done == false
+            {
+                itemArray[indexPath.row].done = true
+                
+            }
+            
+            else
+            {
+                itemArray[indexPath.row].done = false
+            }
+                    
+            tableView.reloadData()
+         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+     func addNewItem(_ sender: UIBarButtonItem) {
+        
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "New Item", message: "", preferredStyle: .alert)
+          let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            self.defults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.tableView.reloadData()
+              
+    }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+      override  func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            itemArray.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+
+    
+    }
+
+
+
+
+
---
+++ DoneGone/DataModel/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  DoneGone
+//
+//  Created by Shahzod Buriev on 2/5/20.
+//  Copyright © 2020 Shahzod Buriev. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    
+    var title : String = ""
+    var done : Bool = false
+    
+   
+    
+}
